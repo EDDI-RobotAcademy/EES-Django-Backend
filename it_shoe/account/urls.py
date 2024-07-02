448b6691831513c213ff2ff8b3bab0b66a4779eb
moneyandjelly <rmsdldy12@gmail.com>
2024-07-02 17:08:37 +0900

    add: [이근] 닉네임 반환 기능 추가 [EESD-33]

--- it_shoe/account/urls.py
+++ it_shoe/account/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('nickname-duplication-check',
          AccountView.as_view({'post': 'checkNicknameDuplication'}), name='account-nickname-duplication-check'),
     path('register', AccountView.as_view({'post': 'registerAccount'}), name='register-account'),
+    path('nickname', AccountView.as_view({'post': 'getNickname'}),name='nickname-account'),
 ]
