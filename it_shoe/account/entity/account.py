e2d227d017d83542c7127158f89b98b07a220c64
Yoonseoparkk <patpark97@gmail.com>
2024-07-05 14:54:06 +0900

    add: [박윤서] 계정 db entity 요소 추가 [EESD-23]

--- it_shoe/account/entity/account.py
+++ it_shoe/account/entity/account.py
@@ -8,6 +8,8 @@ class Account(models.Model):
     id = models.AutoField(primary_key=True)
     loginType = models.ForeignKey(AccountLoginType, on_delete=models.CASCADE)
     roleType = models.ForeignKey(AccountRoleType, on_delete=models.CASCADE)
+    regDate = models.DateTimeField(auto_now_add=True)
+    updDate = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Account -> id: {self.id}, loginType: {self.loginType}, roleType: {self.roleType}"
--- it_shoe/account/entity/account_role_type.py
+++ it_shoe/account/entity/account_role_type.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from account.entity.role_type import RoleType
+
 
 class AccountRoleType(models.Model):
-    roleType = models.CharField(max_length=64)
+    roleType = models.CharField(max_length=64, choices=RoleType.choices)
 
     def __str__(self):
         return self.roleType
