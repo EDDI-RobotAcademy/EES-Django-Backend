306c6d358ee669c02afd16326f52cb6ea9c103d2
Gomin <138251577+MinMessi@users.noreply.github.com>
2024-07-08 17:21:21 +0900

    Fix: Unable to Pull

--- it_shoe/orders/controller/views.py
+++ it_shoe/orders/controller/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-
-from orders.service.orders_service_impl import OrdersServiceImpl
-
 from kakao_oauth.service.redis_service_impl import RedisServiceImpl
+from orders.service.orders_service_impl import OrdersServiceImpl
 
 
 class OrdersView(viewsets.ViewSet):
@@ -51,4 +49,25 @@ class OrdersView(viewsets.ViewSet):
 
         except Exception as e:
             print('주문 상세 내역 조회 중 문제 발생:', e)
+            return Response({ 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def ordersList(self, request):
+        try:
+            data = request.data
+            userToken = data.get('userToken')
+            accountId = self.redisService.getValueByKey(userToken)
+            print(f"accountId: {accountId}")
+
+            if not accountId:
+                raise ValueError('Invalid userToken')
+
+            page = data.get('page')
+            print(f"page: {page}")
+
+            ordersList = self.ordersService.ordersList(accountId, data)
+
+            return Response(ordersList, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print('주문 리스트 조회 중 문제 발생:', e)
             return Response({ 'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
